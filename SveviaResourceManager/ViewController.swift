93562e9796b264c26d245cb4d08a7572af343b5b
Björn Holmér <bjorn.holmer@gmail.com>
2018-10-04 01:54:38 +0200

    Initial Commit

---
+++ SveviaResourceManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SveviaResourceManager
+//
+//  Created by Björn Holmér on 2018-10-04.
+//  Copyright © 2018 Seizemore Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
